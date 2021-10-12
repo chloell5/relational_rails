ead099553131f99c04f25fb41964bf1cf058a1eb
ChloeLL <chloell@protonmail.ch>
2021-10-11 19:00:25 -0700

    Add new views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+
+  get 'museums', to: 'museums#index'
+
+  get 'compositions', to: 'compositions#index'
 end
