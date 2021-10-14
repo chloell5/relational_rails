a31b85fa9d8f22fbe16b2db770716ad10796f9e8
ChloeLL <chloell@protonmail.ch>
2021-10-14 14:35:23 -0700

    Refactor spec files

--- spec/features/compositions/index_spec.rb
+++ spec/features/compositions/index_spec.rb
@@ -1,45 +1,43 @@
 require 'rails_helper'
 
 describe 'compositions index page' do
-  before(:each) do
-    @museum = Museum.create!(
+  it 'shows all composition details' do
+    museum = Museum.create!(
       name: 'The Louvre',
       free_admission: false,
       donation_revenue: 12345678
     )
-    @museum2 = Museum.create!(
+    museum2 = Museum.create!(
       name: 'MOMA',
       free_admission: true,
       donation_revenue: 7654321
     )
 
-    @comp = Composition.create!(
+    comp = Composition.create!(
       name: 'Mona Lisa',
       artist: 'da Vinci',
       on_display: true,
       year_made: 1503,
-      museum_id: @museum.id
+      museum_id: museum.id
     )
 
-    @comp2 = Composition.create!(
+    comp2 = Composition.create!(
       name:'The Persistence of Memory',
       artist:'Dali',
       on_display: false,
       year_made: 1931,
-      museum_id: @museum2.id
+      museum_id: museum2.id
     )
 
     visit '/compositions'
-  end
 
-  it 'shows all composition details' do
-    expect(page).to have_content(@comp.name)
-    expect(page).to have_content(@comp.artist)
-    expect(page).to have_content(@comp.on_display)
-    expect(page).to have_content(@comp.year_made)
-    expect(page).to have_content(@comp2.name)
-    expect(page).to have_content(@comp2.artist)
-    expect(page).to have_content(@comp2.on_display)
-    expect(page).to have_content(@comp2.year_made)
+    expect(page).to have_content(comp.name)
+    expect(page).to have_content(comp.artist)
+    expect(page).to have_content(comp.on_display)
+    expect(page).to have_content(comp.year_made)
+    expect(page).to have_content(comp2.name)
+    expect(page).to have_content(comp2.artist)
+    expect(page).to have_content(comp2.on_display)
+    expect(page).to have_content(comp2.year_made)
   end
 end
