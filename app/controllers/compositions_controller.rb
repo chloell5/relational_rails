66226e37d1a1118f55322290e50f5936c31d31bd
ChloeLL <chloell@protonmail.ch>
2021-10-14 15:34:32 -0700

    Remove extraneous code

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -3,16 +3,7 @@ class CompositionsController < ApplicationController
     @compositions = Composition.all
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def show
     @composition = Composition.find(params[:id])
-  end
-
-  def edit
   end
 end
