5be6f6965ebbc3e33de942b2271679f936751faa
Stephanie Helm <stephaniemhelm@gmail.com>
2021-10-16 13:53:05 -0600

    refactor gyms controller

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -7,9 +7,6 @@ class GymsController < ApplicationController
   def show
     @gym = Gym.find(params[:id])
     @activity_count = @gym.activities.count
-
-    # @activity_count = Gym.find(params[:id]).activities.count
-    # require "pry"; binding.pry
   end
 
 end
