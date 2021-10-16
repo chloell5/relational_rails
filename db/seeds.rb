6bebd4a214cc99d1aefa84ae59ded8af56f4fd96
Stephanie Helm <stephaniemhelm@gmail.com>
2021-10-15 20:24:44 -0600

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,45 +11,45 @@
 require 'factory_bot_rails'
 
 #Gym.destroy_all
-# 3.times do
-#   FactoryBot.create(:gym)
-# end
+3.times do
+  FactoryBot.create(:gym)
+end
 
 5.times do
   FactoryBot.create(:activity)
 end
 
-# @museum = Museum.create!(
-#   name: 'The Louvre',
-#   free_admission: false,
-#   donation_revenue: 12345678
-# )
-# @museum2 = Museum.create!(
-#   name: 'MOMA',
-#   free_admission: true,
-#   donation_revenue: 7654321
-# )
-#
-# @comp = Composition.create!(
-#   name: 'Mona Lisa',
-#   artist: 'da Vinci',
-#   on_display: true,
-#   year_made: 1503,
-#   museum_id: @museum.id
-# )
-#
-# @comp2 = Composition.create!(
-#   name:'The Persistence of Memory',
-#   artist:'Dali',
-#   on_display: false,
-#   year_made: 1931,
-#   museum_id: @museum2.id
-# )
-#
-# @comp3 = Composition.create!(
-#   name:'The Starry Night',
-#   artist:'van Gogh',
-#   on_display: true,
-#   year_made: 1889,
-#   museum_id: @museum.id
-# )
+@museum = Museum.create!(
+  name: 'The Louvre',
+  free_admission: false,
+  donation_revenue: 12345678
+)
+@museum2 = Museum.create!(
+  name: 'MOMA',
+  free_admission: true,
+  donation_revenue: 7654321
+)
+
+@comp = Composition.create!(
+  name: 'Mona Lisa',
+  artist: 'da Vinci',
+  on_display: true,
+  year_made: 1503,
+  museum_id: @museum.id
+)
+
+@comp2 = Composition.create!(
+  name:'The Persistence of Memory',
+  artist:'Dali',
+  on_display: false,
+  year_made: 1931,
+  museum_id: @museum2.id
+)
+
+@comp3 = Composition.create!(
+  name:'The Starry Night',
+  artist:'van Gogh',
+  on_display: true,
+  year_made: 1889,
+  museum_id: @museum.id
+)
