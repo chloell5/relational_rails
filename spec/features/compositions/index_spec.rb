cb7bc0e84e8337e4e52be187495276f475dd70e9
ChloeLL <chloell@protonmail.ch>
2021-10-18 00:36:54 -0700

    Modify composition index to show only on_display true entries

--- spec/features/compositions/index_spec.rb
+++ spec/features/compositions/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'compositions index page' do
-  it 'shows all composition details' do
+  it 'shows all composition details, if on display is true' do
     museum = Museum.create!(
       name: 'The Louvre',
       free_admission: false,
@@ -35,10 +35,10 @@ describe 'compositions index page' do
     expect(page).to have_content(comp.artist)
     expect(page).to have_content(comp.on_display)
     expect(page).to have_content(comp.year_made)
-    
-    expect(page).to have_content(comp2.name)
-    expect(page).to have_content(comp2.artist)
-    expect(page).to have_content(comp2.on_display)
-    expect(page).to have_content(comp2.year_made)
+
+    expect(page).to_not have_content(comp2.name)
+    expect(page).to_not have_content(comp2.artist)
+    expect(page).to_not have_content(comp2.on_display)
+    expect(page).to_not have_content(comp2.year_made)
   end
 end
