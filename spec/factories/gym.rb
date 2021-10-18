530481a4c924e7cc044281b7cd16421ab8119aa3
Stephanie Helm <stephaniemhelm@gmail.com>
2021-10-18 12:22:56 -0600

    update method and refactor factory bot

--- spec/factories/gym.rb
+++ spec/factories/gym.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :gym, class: Gym do
-    name {["Peak Fitness", "Yoga Olas", "Yoga Tonic", "BV Dance Studio", "Local Gym"].sample}
+    #name {["Fitness and Yoga", "Yoga Olas", "Rec Center of the Rockies", "BV Dance Studio", "Local Gym"].sample}
+    name {[Faker::Company.name].uniq.sample}
     location {Faker::Address.city}
     open {Faker::Boolean.boolean}
     rank {Faker::Number.between(from: 1, to: 20)}
